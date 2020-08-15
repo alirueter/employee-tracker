205795027749cc26c2fa0175de54ec12425d194d
alirueter <rueter.alison@gmail.com>
2020-08-15 18:36:31 -0500

    updated seeds.sql

--- db/database.js
+++ db/database.js
@@ -3,14 +3,25 @@ const connection = require('./connection');
 class DB {
     createEmployee(employee) {
         return this.connection.promise().query("INSERT INTO employees SET ?", employee);
-    }
+    };
     createRole(role) {
         return this.connection.promise().query("INSERT INTO role SET ?", role);
-    }
+    };
     createDepartment(department) {
         return this.connection.promise().query("INSERT INTO department SET ?", department);
+    };
+    viewDepartments() {
+        return this.connection.promise().query("SELECT * FROM department");
     }
-    
+    viewRoles() {
+        return this.connection.promise().query("SELECT * FROM role"); 
+    }
+    viewEmployees() {
+        return this.connection.promise().query("SELECT * FROM employees");
+    }
+    updateRole(role_id, first_name){
+        return this.connection.promise().query("UPDATE employees SET role_id = ? WHERE first_name = ?", role_id, first_name);
+    }
 }
 
 module.exports = new DB(connection);
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const inquirer = require('inquirer');
 const db = require("./db");
+const mysql = require('mysql2');
+const cTable = require('console.table');
 
 //what do you want to do (actions within database.js)
 //ask info you need
